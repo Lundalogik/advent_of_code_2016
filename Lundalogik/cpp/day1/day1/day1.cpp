b87e75ad13b9c0a1599e7044d8bebf5eed31628e
MHE <mikael.hernrup@lundalogik.se>
2016-12-20 12:58:30 +0100

    Day 1 - part 2. in cpp

--- Lundalogik/cpp/day1/day1/day1.cpp
+++ Lundalogik/cpp/day1/day1/day1.cpp
@@ -14,22 +14,30 @@
 using namespace std;
 
 const string DIRECTIONS = "L1, R3, R1, L5, L2, L5, R4, L2, R2, R2, L2, R1, L5, R3, L4, L1, L2, R3, R5, L2, R5, L1, R2, L5, R4, R2, R2, L1, L1, R1, L3, L1, R1, L3, R5, R3, R3, L4, R4, L2, L4, R1, R1, L193, R2, L1, R54, R1, L1, R71, L4, R3, R191, R3, R2, L4, R3, R2, L2, L4, L5, R4, R1, L2, L2, L3, L2, L1, R4, R1, R5, R3, L5, R3, R4, L2, R3, L1, L3, L3, L5, L1, L3, L3, L1, R3, L3, L2, R1, L3, L1, R5, R4, R3, R2, R3, L1, L2, R4, L3, R1, L1, L1, R5, R2, R4, R5, L1, L1, R1, L2, L4, R3, L1, L3, R5, R4, R3, R3, L2, R2, L1, R4, R2, L3, L4, L2, R2, R2, L4, R3, R5, L2, R2, R4, R5, L2, L3, L2, R5, L4, L2, R3, L5, R2, L1, R1, R3, R3, L5, L2, L2, R5";
+
 void removeCharsFromString(string &str, char* charsToRemove) {
 	for (unsigned int i = 0; i < strlen(charsToRemove); ++i) {
 		str.erase(remove(str.begin(), str.end(), charsToRemove[i]), str.end());
 	}
 }
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+struct coordinate{
+	int x;
+	int y;
+};
+
+int _tmain(int argc, _TCHAR* argv[]) {
 	istringstream iss(DIRECTIONS);
 	vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}};
 	int x = 0;
 	int y = 0;
+	vector<coordinate> coordinates;
+	coordinate current_position;
 	string token;
 	char dir;
 	int steps;
 	int current_dir = 0;
+	bool foundit = false;
 
 	for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); ++it) {
 		token = *it;
@@ -45,31 +53,36 @@ int _tmain(int argc, _TCHAR* argv[])
 			current_dir = (current_dir + 4 - 1) % 4;
 		}
 
-		switch (current_dir){
-		case 0:
-			y += steps;
-			break;
-		case 1:
-			x += steps;
-			break;
-		case 2:
-			y -= steps;
-			break;
-		case 3:
-			x -= steps;
-			break;
-		}
+		for (int i = 0; i < steps; i++){
+			switch (current_dir){
+			case 0:
+				y++;
+				break;
+			case 1:
+				x++;
+				break;
+			case 2:
+				y--;
+				break;
+			case 3:
+				x--;
+				break;
+			}
 
-	/*		
-		std::cout << "---------\n";
-		std::cout << token << "\n";
-		std::cout << current_dir << "\n";
-		std::cout << x << "\n";
-		std::cout << y << "\n";
-	*/	
+			if (!foundit){
+				current_position = coordinate{ x = x, y = y };
+				for (vector<coordinate>::iterator c = coordinates.begin(); c != coordinates.end(); ++c){
+					if (c->x == x && c->y == y){
+						foundit = true;
+						std::cout << "Found first common location at x:" << x << ", y:" << y << ". Distance:" << abs(y) + abs(x) << "\n";
+					}
+				}
+				coordinates.push_back(current_position);
+			}
+		}
 	}
 
-	std::cout << "Answer:" << abs(y)+abs(x) << "\n";
+	std::cout << "Distance to the big bad bunny:" << abs(y)+abs(x) << "\n";
 }
 
 
